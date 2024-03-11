106ba5a46a05dc6f33a053f4c045c71e78965dee
HarishG03 <131017755+HarishG03@users.noreply.github.com>
2024-03-11 22:47:22 +0530

    Path updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/gadgets', (request, response) => {
-  response.sendFile('./gadgets.html/', {root: __dirname})
+  response.sendFile('./gadgets.html', {root: __dirname})
 })
 
 app.listen(3000)
